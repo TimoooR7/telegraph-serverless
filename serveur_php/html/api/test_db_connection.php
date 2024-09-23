7e71a828ccd8dff18969f19f4b7e3938f68c5b7c
Timo Rouwenhorst <timo.rouwenhorst@eduvaud.ch>
2024-09-23 09:23:08 +0200

    Upload du telegraphe

---
+++ serveur_php/html/api/test_db_connection.php
@@ -0,0 +1,16 @@
+<?php
+    $servername = "192.168.1.140:6033";
+    $username = "root";
+    $password = "root";
+    $dbname = "db_telegraph";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Test réussi, la base de données db_telegraph est accessible";
+    } 
+    catch(PDOException $e) {
+        echo "Test échoué, la base de données db_telegraph est inaccessible: " . $e->getMessage();
+    }
+?>
